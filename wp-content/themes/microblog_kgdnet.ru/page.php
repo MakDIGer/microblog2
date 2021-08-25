cf8f8dcc68ad5a8e7a39647ce0d0c61026211dfd
Makarin Nikolay <makarin@kgdnet.ru>
2021-08-25 20:52:39 +0200

    about-page completed.

--- wp-content/themes/microblog_kgdnet.ru/page.php
+++ wp-content/themes/microblog_kgdnet.ru/page.php
@@ -2,6 +2,17 @@
 
 get_header();
 
-get_template_part( 'sidebar' );
+?>
+
+<section class="post">
+    <h3 class="title__post"><a href="/about" class="link">&#123; // О проекте // }</a></h3>
+    <p class="text__post mt-4">
+        <?php the_content(); ?>
+    </p>
+</section>
+
+<?php
+
+get_sidebar();
 
 get_footer();
